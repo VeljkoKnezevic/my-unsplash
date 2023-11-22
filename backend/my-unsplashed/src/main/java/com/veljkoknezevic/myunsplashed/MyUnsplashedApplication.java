3cd673e7d5bea164d81f2fe5c144145971c6cc25
VeljkoKnezevic <veljkoknedla@gmail.com>
2023-11-22 15:18:10 +0100

    First commit backend

---
+++ backend/my-unsplashed/src/main/java/com/veljkoknezevic/myunsplashed/MyUnsplashedApplication.java
@@ -0,0 +1,13 @@
+package com.veljkoknezevic.myunsplashed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyUnsplashedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyUnsplashedApplication.class, args);
+    }
+
+}
---
+++ backend/my-unsplashed/src/test/java/com/veljkoknezevic/myunsplashed/MyUnsplashedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veljkoknezevic.myunsplashed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyUnsplashedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
